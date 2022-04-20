8ec3190be4d1ea097b3b1f755e5c23ab757eda18
JackRB52 <jbouchard@chicagobooth.edu>
2022-04-20 03:43:44 +0000

    start of forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
 
+  def blank_square_form
+    render({ :template => "calculation_templates/square_form.html.erb"})
+  end
+
+  def calculate_square
+    @num = params.fetch("value").to_f
+    @square_of_num = @num ** 2
+    render({ :template => "calculation_templates/square_results.html.erb"})
+  end
+
+  def blank_random_form
+    render({ :template => "calculation_templates/random_form.html.erb"})
+  end
+
+  def calculate_rand
+    @min = params.fetch("min_val").to_f
+    @max = params.fetch("max_val").to_f
+    @rand = rand(@min..@max)
+    render({ :template => "calculation_templates/rand_results.html.erb"})
+  end
+
 end
