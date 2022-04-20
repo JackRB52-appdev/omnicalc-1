dbfd96625131a63ee0312893b38dabd0367fbd31
JackRB52 <jbouchard@chicagobooth.edu>
2022-04-20 19:38:01 +0000

    first form style app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,4 +21,36 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/rand_results.html.erb"})
   end
 
+  def blank_pay_form
+    render({ :template => "calculation_templates/pay_form.html.erb"})
+  end
+
+  def calculate_pay
+    @apr = params.fetch("apr").to_f/100
+    @apr_mthly = @apr/12
+    @apr_rd = (@apr*100).round(4)
+    @apr_display = (@apr_rd).to_s + "%"
+    
+    @years = params.fetch("years").to_i
+    @mths = @years * 12
+
+    @principal = params.fetch("principal").to_f
+    @principal_display = @principal.to_s(:currency)
+
+    @monthly_pay = @principal*@apr_mthly*(1 + @apr_mthly)**@mths/((1+@apr_mthly)**@mths - 1)
+    @mthly_pay_display = @monthly_pay.to_s(:currency)
+
+    render({ :template => "calculation_templates/pay_results.html.erb"})
+  end
+
+  def blank_square_root_form
+    render({ :template => "calculation_templates/root_form.html.erb"})
+  end
+
+  def calculate_square_root
+    @val = params.fetch("value").to_f
+    @root = @val**0.5
+
+    render({ :template => "calculation_templates/root_results.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "application", :action => "blank_square_form"})
   get("/square/new", { :controller => "application", :action => "blank_square_form"}  )
-  get("/random/new", { :controller => "application", :action => "blank_random_form"}  )
   get("/square/results", { :controller => "application", :action => "calculate_square"}  )
+  get("/random/new", { :controller => "application", :action => "blank_random_form"}  )
   get("/random/results", { :controller => "application", :action => "calculate_rand"}  )
+  get("/payment/new", {:controller => "application", :action => "blank_pay_form"})
+  get("/payment/results", {:controller => "application", :action => "calculate_pay"})
+  get("/square_root/new", {:controller => "application", :action => "blank_square_root_form"})
+  get("/square_root/results", {:controller => "application", :action => "calculate_square_root"})
 
 end
